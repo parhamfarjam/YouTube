0c81336cdc843d1b2337a09c8f74cb1fcffcb929
karim farjam <karimfarjam@gmail.com>
2023-01-29 15:21:23 +0100

    youtube

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { Box } from '@mui/material'
+import Feed from './components/Feed'
+import VideoDetail from './components/VideoDetail'
+import ChannelDetail from './components/ChannelDetail'
+import SearchFeed from './components/SearchFeed'
+import NavBar from './components/NavBar'
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+        <Box sx={{background: '#000'}}>
+            <NavBar/>
+            <Routes>
+                <Route path='/' element={<Feed/>} />
+                <Route path='/video/:id' element={<VideoDetail/>} />
+                <Route path='/channel/:id' element={<ChannelDetail/>} />
+                <Route path='/search/:searchTerm' element={<SearchFeed/>} />
+            </Routes>
+        </Box>
+    </BrowserRouter>
+  )
 }
-
-export default App;
